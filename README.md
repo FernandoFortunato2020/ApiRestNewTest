18ed68c712581185bb2cbe80a1ef5de747c570be
FernandoFortunato2020 <65669290+FernandoFortunato2020@users.noreply.github.com>
2023-09-05 12:26:26 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# ApiRestAutomacaoTest!
+
+Este repositório engloba a automatização de testes de API Rest, abrangendo operações como GET, POST, PUT e Delete, para um site acessível através do link: https://jsonplaceholder.typicode.com/users. Os subitens a seguir detalham as escolhas cruciais realizadas durante a fase de estruturação deste projeto.
+
+
+# Tecnologia Utilizadas
+- Java
+  (https://download.oracle.com/java/17/latest/jdk-17_windows-x64_bin.exe)
+- Junit
+  (https://mvnrepository.com/artifact/org.junit.jupiter/junit-jupiter-api)
+- RestAssured
+  (https://mvnrepository.com/artifact/io.rest-assured/rest-assured)
+- Jackson Databind
+  (https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-databind)
+- Maven
+  (https://maven.apache.org/)
+
+
+## Testes Automatizados
+
+Testes destinados a verificar os verbos (GET, POST, PUT e Delete) estão intrinsecamente associados a um desafio que busca novas oportunidades.
+
+GET (Retorno do Status Code 200 OK)
+
+POST (Retorno do Status Code 201 Created)
+
+PUT (Retorno do Status Code 200 OK)
+
+Delete (Retorno do Status Code 200 OK)
+
+
+## Notas Gerais
+
+No âmbito deste projeto, utilizamos o Junit 5, o qual nos proporciona a capacidade de empregar a anotação DisplayName para fornecer descrições em português aos nossos testes.
---
+++ src/test/java/VerbosTest.java
@@ -0,0 +1,109 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.equalTo;
+
+@DisplayName("Testes de API Rest")
+public class VerbosTest {
+
+    private static final String BASE_URI = "https://jsonplaceholder.typicode.com";
+    private static final String BASE_PATH = "/users/10";
+
+    @BeforeEach
+    public void beforeEach() {
+        baseURI = BASE_URI;
+        basePath = BASE_PATH;
+    }
+
+    @Test
+    public void testGet() {
+        given()
+                .when()
+                .get("https://jsonplaceholder.typicode.com/users?id=10")
+                .then()
+                .body("name", contains("Clementina DuBuque"))
+                .body("email", contains("Rey.Padberg@karina.biz"))
+                .body("username", contains("Moriah.Stanton"))
+                .body("phone", contains("024-648-3804"))
+                .body("website", contains("ambrose.net"))
+                .statusCode(200);
+    }
+
+    @Test
+    public void testPost() {
+        given()
+                .body("{\n" +
+                        "        \"id\": 1,\n" +
+                        "        \"name\": \"Fernando Fortunato\",\n" +
+                        "        \"username\": \"FerFortunato\",\n" +
+                        "        \"email\": \"fernandofortunato@hotmail.com\",\n" +
+                        "        \"address\": {\n" +
+                        "            \"street\": \"Frederico Maurer\",\n" +
+                        "            \"suite\": \"Apt. 208\",\n" +
+                        "            \"city\": \"Curitiba\",\n" +
+                        "            \"zipcode\": \"81630-020\",\n" +
+                        "            \"geo\": {\n" +
+                        "                \"lat\": \"-40.7779\",\n" +
+                        "                \"lng\": \"80.8886\"\n" +
+                        "            }\n" +
+                        "        },\n" +
+                        "        \"phone\": \"5-123-876-1111 x56442\",\n" +
+                        "        \"website\": \"teste.org\",\n" +
+                        "        \"company\": {\n" +
+                        "            \"name\": \"Parana\",\n" +
+                        "            \"catchPhrase\": \"Multi-layered client-server neural-net\",\n" +
+                        "            \"bs\": \"harness real-time e-markets\"\n" +
+                        "        }\n" +
+                        "    }'")
+                .when()
+                .post("https://jsonplaceholder.typicode.com/users")
+                .then()
+                .statusCode(201)
+                .body("id", equalTo(11));
+
+    }
+
+    @Test
+    public void testPut() {
+        given()
+                .body("{\n" +
+                        "        \"id\": 1,\n" +
+                        "        \"name\": \"Fernando Fortunato Alterado\",\n" +
+                        "        \"username\": \"FerFortunato Alterado\",\n" +
+                        "        \"email\": \"fernandofortunatoalterado@hotmail.com\",\n" +
+                        "        \"address\": {\n" +
+                        "            \"street\": \"Frederico Maurer Alterado\",\n" +
+                        "            \"suite\": \"Apt. 208 Alterado\",\n" +
+                        "            \"city\": \"Curitiba Alterado\",\n" +
+                        "            \"zipcode\": \"81630-020\",\n" +
+                        "            \"geo\": {\n" +
+                        "                \"lat\": \"-40.7779\",\n" +
+                        "                \"lng\": \"80.8886\"\n" +
+                        "            }\n" +
+                        "        },\n" +
+                        "        \"phone\": \"5-123-876-1111 x56442\",\n" +
+                        "        \"website\": \"testealterado.org\",\n" +
+                        "        \"company\": {\n" +
+                        "            \"name\": \"Parana Alterado\",\n" +
+                        "            \"catchPhrase\": \"Multi-layered client-server neural-net\",\n" +
+                        "            \"bs\": \"harness real-time e-markets\"\n" +
+                        "        }\n" +
+                        "    }'")
+                .when()
+                .put()
+                .then()
+                .statusCode(200)
+                .body("id", equalTo(10));
+    }
+
+    @Test
+    public void testDelete() {
+        given()
+                .when()
+                .delete()
+                .then()
+                .statusCode(200);
+    }
+}
